0174c1956165cd64c029543ec4a992bd453156d4
Kazuki Tsukamoto <ktsukamo@c3r6s7.42tokyo.jp>
2024-07-07 18:00:56 +0900

    teset

---
+++ srcs/execpath.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execpath.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktsukamo <ktsukamo@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/07 16:33:23 by ktsukamo          #+#    #+#             */
+/*   Updated: 2024/07/07 17:18:56 by ktsukamo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+void	fatal_error(const char *msg) __attribute__((noreturn));
+
+void	fatal_error(const char *msg)
+{
+	dprintf(2, "Fatal Error: %s\n", msg);
+	exit(1);
+}
+
+int interpret (char *line)
+{
+	extern char **environ;
+	char *argv[] = {line, NULL};
+	pid_t pid;
+	int wstatus;
+
+	pid = fork();
+	if (pid < 0)
+		fatal_error("fork");
+	else if (pid == 0)
+	{
+		execve(line, argv, environ);
+		fatal_error("execve");
+	}
+	else
+	{
+		wait(&wstatus);
+		return()
+	}
+}
